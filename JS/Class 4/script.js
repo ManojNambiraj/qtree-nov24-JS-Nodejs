6149f59cd12325cef691f6118ad90bd902c00417
ManojNambiraj <postboxmano@gmail.com>
2024-11-26 10:50:36 +0530

    Class 5 Arrays and Objects

---
+++ JS/Class 4/script.js
@@ -0,0 +1,23 @@
+// let users = [
+//     { name: 'Anto', gender:'m'},
+//     { name: 'Franklin', gender:'m'},
+//     { name: 'sam', gender:'m'},
+//     { name: 'jasmine', gender:'f'},
+// ]
+
+
+// for (const user of users) {
+//     console.log(user)
+// }
+
+// if (targetContactFlowData.Contactflow.Content?.Actions){
+//     logger.info("coming inside if condition")
+// }
+
+const user = {
+    dog: {
+        name: "ALEX"
+    }
+}
+
+console.log(user.dog?.name);
---
+++ JS/Class 5/script.js
@@ -0,0 +1,61 @@
+// Array:
+
+    // let arr = [10, 20, 30, 40, 50, 60, 70]
+
+    // let arr = ["apple", "orange","banana", "kiwi", "dragon"]
+        //     #0  #1  #2  #3  #4  #5  #6
+    
+        // for(let i = 0; i < arr.length; i++){
+        //     console.log(arr[i]);
+        // }
+
+    // for in
+
+        // for(let item in arr){
+        //     console.log(arr[item]);  
+        // }
+
+    // for of
+
+        // for(let item of arr){
+        //     console.log(item);
+        // }
+    
+// Objects:  {key: value}
+
+    // let student = {
+    //   name: "ravi",
+    //   age: 25,
+    //   college: "xyz",
+    //   dept: "abc",
+    //   mobile: 9876543211,
+    //   favColor: ["black", "red", "blue"],
+    //   address: {
+    //     dno: 2,
+    //     street: "laksmi street",
+    //     landmark: "watertank"
+    //   }
+    // };
+
+    // console.log(student.favColor[1]);
+    // console.log(student.address.landmark);
+    
+// Object Destructuring: (Unpacking)
+
+    // const {name, age, college, dept} = student
+    
+    // console.log(name);
+    // console.log(age);
+    // console.log(college);
+
+// Array Destructuring:
+
+    // let arr = ["apple", "orange","banana", "kiwi", "dragon"]
+
+    // const [a, o, b, k, d] = arr
+
+    // console.log(k);
+
+    // console.log(arr);
+    // console.log(arr[1]);
+    // console.log(arr[3]);
