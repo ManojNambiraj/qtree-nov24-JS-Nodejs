21094c73e692e47613a2526ff2a721ac00db673c
ManojNambiraj <postboxmano@gmail.com>
2024-11-27 19:30:51 +0530

    class 6 codes

---
+++ JS/Class 6/script.js
@@ -0,0 +1,34 @@
+// Functions:
+
+    // 1. Built-in functions
+    // 2. User-defined functions
+
+// a) Normal function
+
+    // function Mul(x, y){
+    //     return x * y
+    // }
+
+    // console.log(Mul(100, 5));
+
+// b) Ananymous function  -> Unnamed
+
+    // let anay = function(a){
+    //     console.log(a);
+    // }
+
+    // anay("hi")
+
+// c) Arrow Function
+
+    // let arr = (a, b) => a + b
+    
+    // let result = arr(20, 5)
+
+    // console.log(result)
+    
+// d) IIFE Function (Immediatly Invoked Function Expression)
+
+    // (() => {
+    //     console.log("I'm IIFE");
+    // })()
